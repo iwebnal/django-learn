1b19c3d28a9812a1a6defe493446b3150acf1fed
iwebnal <iwebnal@gmail.com>
2023-11-05 10:19:51 +0300

    Add settings for static files in debug process

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from coolsite import settings
 from women.views import *
 
 urlpatterns = [
@@ -25,5 +27,8 @@ urlpatterns = [
     # path('cats/', categories),  # http://127.0.0.1:8000/cats/
     path('', include('women.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
