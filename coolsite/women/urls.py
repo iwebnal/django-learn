26b4ab3e9b7a7c2e884519c24db8c0b76dfcfaba
iwebnal <iwebnal@gmail.com>
2023-11-05 12:48:31 +0300

    Add templates for our site

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),  # http://127.0.0.1:8000/
     path('cats', categories),  # http://127.0.0.1:8000/cats
+    path('about/', about, name='about'),  # http://127.0.0.1:8000/about
     path('cat/<int:catid>/', category),  # http://127.0.0.1:8000/cat/1
     path('cat-slug/<slug:cat>/', category_slug),  # http://127.0.0.1:8000/cat-slug/1
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),  # http://127.0.0.1:8000/archive/2020/
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+# def index(request):
+#     return HttpResponse("Страница приложения Women")
+
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
 
 def index(request):
-    return HttpResponse("Страница приложения Women")
+    return render(request, 'women/index.html', {'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
 def categories(request):
