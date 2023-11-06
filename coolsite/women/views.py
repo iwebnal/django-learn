1d5bd034c1d2acacdbc455a6157291549945b929
iwebnal <iwebnal@gmail.com>
2023-11-06 14:18:32 +0300

    Page not found for categories

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -58,6 +58,10 @@ def show_post(request, post_id):
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
     cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404
+
     context = {
         'posts': posts,
         'cats': cats,
