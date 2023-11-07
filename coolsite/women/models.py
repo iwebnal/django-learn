de59db7057a5635c076b5fc1cf4c14b71c4f5d89
iwebnal <iwebnal@gmail.com>
2023-11-07 08:14:52 +0300

    Configure admin panel view

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -17,6 +17,11 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Известные юзеры'
+        verbose_name_plural = 'Известные юзеры'
+        ordering = ['time_create', 'title']
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
