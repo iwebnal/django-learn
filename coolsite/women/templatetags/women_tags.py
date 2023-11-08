aeabf87c1c1beeb2746515916b609594868aae4a
iwebnal <iwebnal@gmail.com>
2023-11-09 00:02:23 +0300

    Passing a parameter to a simple tag

--- coolsite/women/templatetags/women_tags.py
+++ coolsite/women/templatetags/women_tags.py
@@ -5,8 +5,11 @@ register = template.Library()
 
 
 @register.simple_tag(name='getcats')
-def get_categories():
-    return Category.objects.all()
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
 
 
 @register.inclusion_tag('women/list_categories.html')
