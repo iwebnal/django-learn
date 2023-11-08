8a2b01c9c2efb40a868cd4b5bbe24d09971b9300
iwebnal <iwebnal@gmail.com>
2023-11-08 23:54:14 +0300

    Create including tag

--- coolsite/women/templatetags/women_tags.py
+++ coolsite/women/templatetags/women_tags.py
@@ -7,3 +7,9 @@ register = template.Library()
 @register.simple_tag(name='getcats')
 def get_categories():
     return Category.objects.all()
+
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories():
+    cats = Category.objects.all()
+    return {'cats': cats}
