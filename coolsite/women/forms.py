ed56264f188120fd5d2ee7f599dec70f4a366e80
iwebnal <iwebnal@gmail.com>
2023-11-14 00:48:25 +0300

    Refactoring form for add page. Add styles too

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -3,8 +3,8 @@ from .models import *
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-    is_published = forms.BooleanField()
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=255, label="Заголовок", widget=forms.TextInput(attrs={'class': 'form-input'}))
+    slug = forms.SlugField(max_length=255, label="URL")
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label="Текст")
+    is_published = forms.BooleanField(label="Публикация", required=False, initial=True)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории", empty_label="Категория не выбрана")
